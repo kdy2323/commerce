03bf7d870eefeb09311802aeb063540b08e8c54b
Kendy_Christian <bib.kendy17@gmail.com>
2025-02-20 15:47:32 +0100

    2eme commit

--- admin/src/php/utils/all_includes.php
+++ admin/src/php/utils/all_includes.php
@@ -7,18 +7,8 @@ if (file_exists('./src/php/db/db_pg_connect.php')) {
     //si on se trouve dans la partie publique
     if (file_exists('./admin/src/php/db/db_pg_connect.php')) {
         require './admin/src/php/db/db_pg_connect.php';
-<<<<<<< HEAD
-        //CES LIGNES SERONT DEPLACEES DANS UNE CLASSE ULTERIEUREMENT
-        $cnx = new PDO($dsn, $user, $password);
-        if($cnx){
-            print "Connection successful";
-        }else {
-            print "Connection error";
-        }
-=======
         require './admin/src/php/classes/Autoloader.class.php';
         Autoloader::register();
         $cnx=Connection::getInstance($dsn,$user,$password);
->>>>>>> 759c3c7 (2eme commit)
     }
 }
